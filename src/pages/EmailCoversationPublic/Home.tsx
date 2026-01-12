9ee3f45d24fb0a441c806d10fc2a3f3134a0dc97
DexterPogi1 <dexterjamero@beesee.ph>
2026-01-12 13:58:36 +0800

    updated_email_conversation

--- src/pages/EmailCoversationPublic/Home.tsx
+++ src/pages/EmailCoversationPublic/Home.tsx
@@ -344,7 +344,7 @@ export default function EmailConversationApp() {
         <div className="flex justify-between items-center p-4 bg-white border-b border-gray-200">
           <div>
             <h2 className="text-md font-semibold text-gray-900">
-              {userTicketInformation.company || 'No Subject'}
+              {userTicketInformation.company || 'Support Team'}
             </h2> 
           </div>
 
