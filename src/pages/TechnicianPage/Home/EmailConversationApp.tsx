91a5cf2c10d4dd0e875a1211eac85e80cc571368
DexterPogi1 <dexterjamero@beesee.ph>
2026-01-12 11:54:37 +0800

    update header

--- src/pages/TechnicianPage/Home/EmailConversationApp.tsx
+++ src/pages/TechnicianPage/Home/EmailConversationApp.tsx
@@ -432,7 +432,7 @@ export default function EmailConversationApp() {
           <div className="flex justify-between items-center p-4 bg-white border-b border-gray-200">
             <div>
               <h2 className="text-md font-semibold text-gray-900">
-                {userTicketInformation.issue_name || 'No Subject'}
+                {userTicketInformation.company || 'No Subject'}
               </h2>
               {/* <p className="text-sm text-gray-500">
                 Conversation with {userTicketInformation.email}
