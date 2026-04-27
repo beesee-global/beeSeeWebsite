3117e74ff4e0f7125489849cc394c10116c1057d
DexterPogi1 <dexterjamero@beesee.ph>
2026-04-27 14:33:14 +0800

    jbo post

--- src/pages/TechnicianPage/JobPosting/JobPosting.tsx
+++ src/pages/TechnicianPage/JobPosting/JobPosting.tsx
@@ -45,8 +45,8 @@ const JobPosting = () => {
     { id: 'work_location', label: 'Work Location', sortable: true, align: 'left' }, 
     { id: 'location', label: 'Location', sortable: true, align: 'left' },
     { id: 'num_applicant', label: "No. Applicant", sortable: true, align: 'left' },
-    { id: 'status', label: "Status", sortable: false, align: 'left' },
-    { id: 'created_at', label: 'Posted Date', sortable: false, align: 'right' }
+    { id: 'status', label: "Status", sortable: true, align: 'left' },
+    { id: 'created_at', label: 'Posted Date', sortable: true, align: 'right' }
   ]
 
   const Permission = userInfo?.permissions?.find(p => p.parent_id === 'careers' && p.children_id === 'job-postings');
