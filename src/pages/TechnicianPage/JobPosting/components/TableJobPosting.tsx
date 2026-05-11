db227ad3d0ed7ef1300bec56aeceebbeb309f7a3
Paulonism <paulo.francisco@beesee.ph>
2026-05-11 16:21:26 +0800

    blue text to green ( 0 applicants )

--- src/pages/TechnicianPage/JobPosting/components/TableJobPosting.tsx
+++ src/pages/TechnicianPage/JobPosting/components/TableJobPosting.tsx
@@ -297,7 +297,7 @@ export default function TableJobPosting({
                                 const count = Number(row.num_applicant);
                                 const classes = count === 0
                                   ? 'bg-red-100 text-red-800 border border-red-200'
-                                  : 'bg-blue-100 text-blue-800 border border-blue-200';
+                                  : 'bg-green-100 text-green-800 border border-green-200';
                                 return (
                                   <span className={`inline-flex items-center ml-2 px-3 py-1 rounded-full text-xs font-medium ${classes}`}>
                                     {count} New Applicants
